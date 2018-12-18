1c93139be88b7abc960e42ba7797002718c5f13c
Louis <tuda.louis@gmail.com>
2018-12-18 12:24:39 +0900

    Revert "gas / gasPrice setting"
    This reverts commit 03b13303552485b59f24a6ec6426f7d964f92040.

--- truffle.js
+++ truffle.js
@@ -1,15 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  rpc: {
+    host: 'localhost',
+    port: '8545'
+  },
   networks: {
-    live: {
-      host: "localhost",
-      port: 9545,
-      network_id: "1",
-      from: "0x82280bd6b6f3806bd5b28a54fd89df941de800b8", // mainNet manage account address
-      gas: 3000000, // gas Limit
-      gasPrice: 3000000000 // gas Price: 2~3Gwei
-    },
     development: {
       host: "127.0.0.1",
       port: 7545,
@@ -21,7 +17,7 @@ module.exports = {
       port: 8545,
       from: "0x2482802cD33e700c28226Cc93fb667769DdC3A20",
       network_id: "*", // Match any network id
-      //gas: 4612388
+      gas: 4612388
       //gas: 7748771
       //gas: 6712390000
     }
